d8000258dc5c5b0d9db371d9d11f7862e69ef189
SrinidhiVangala <33110851+SrinidhiVangala@users.noreply.github.com>
2017-10-26 17:22:21 +0530

    Update B.java

--- B.java
+++ B.java
@@ -3,5 +3,6 @@ public class B{
 public staic void main(String args[]){
 
 System.out.println("second file!!");
-System.out.println("changed from tester!!");
+System.out.println("changed from tester//github!!");
+  
 }}
