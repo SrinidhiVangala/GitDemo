f038d2ce9698f0d23382613f5660a28ff707590f
Vangala <srinidhi.vangala@capgemini.com>
2017-10-26 15:22:44 +0530

    Modified b and test

--- B.java
+++ B.java
@@ -3,5 +3,5 @@ public class B{
 public staic void main(String args[]){
 
 System.out.println("second file!!");
-
+System.out.println("changed from tester!!");
 }}
--- Test.java
+++ Test.java
@@ -3,5 +3,5 @@ public class Test{
 public staic void main(String args[]){
 
 System.out.println("hello!!");
-
+System.out.println("changed from tester!!");
 }}
